f592c6c366a45f78ef4193253230fbdca9bfe91a
Matthew Dempsky <mdempsky@google.com>
2020-12-19 01:44:17 -0800

    polish day19 solution

--- day19/day19.go
+++ day19/day19.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"regexp"
+	"log"
 	"strings"
 
 	advent "github.com/mdempsky/advent2020"
@@ -13,53 +14,132 @@ func main() {
 	rules := paras[0]
 	messages := paras[1]
 
-	rx := compile(rules)
+	cnf1 := compile(rules)
+	cnf2 := compile(append(rules, []string{"8: 42 | 42 8", "11: 42 31 | 42 11 31"}...))
 
-	count := 0
+	count1, count2 := 0, 0
 	for _, message := range messages {
-		if rx.MatchString(message) {
-			count++
+		if match(cnf1, message) {
+			count1++
+		}
+		if match(cnf2, message) {
+			count2++
 		}
 	}
-	fmt.Println("Part 1:", count)
+	fmt.Println("Part 1:", count1)
+	fmt.Println("Part 2:", count2)
 }
 
-func compile(lines []string) *regexp.Regexp {
+type chomsky struct {
+	chars [][]byte
+	pairs [][][2]int
+}
+
+func compile(lines []string) chomsky {
 	rules := map[string]string{}
 	for _, line := range lines {
 		fields := strings.Split(line, ": ")
 		rules[fields[0]] = fields[1]
 	}
 
-	memo := map[string]string{}
-	var eval func(string) string
-	eval = func(key string) string {
-		if res, ok := memo[key]; ok {
-			return res
+	// Compile into Chomsky normal form.
+
+	memo := map[string]int{}
+	var chars [][]byte
+	var pairs [][][2]int
+	var aliases [][]int
+
+	// Eliminate rules with nonsolitary terminals and right-hand sides
+	// with more than 2 nonterminals.
+	var do func(s string) int
+	do = func(s string) int {
+		if x, ok := memo[s]; ok {
+			return x
 		}
-		var buf strings.Builder
-		rule, ok := rules[key]
-		if !ok {
-			panic(key)
+
+		n := len(memo)
+		memo[s] = n
+		chars = append(chars, nil)
+		pairs = append(pairs, nil)
+		aliases = append(aliases, nil)
+
+		if rule, ok := rules[s]; ok {
+			s = rule
 		}
-		buf.WriteString("(")
-		for i, alt := range strings.Split(rule, " | ") {
-			if i > 0 {
-				buf.WriteString("|")
+
+		for _, f := range strings.Split(s, " | ") {
+			if f := strings.SplitN(f, " ", 2); len(f) > 1 {
+				pairs[n] = append(pairs[n], [2]int{do(f[0]), do(f[1])})
+				continue
 			}
-			for _, part := range strings.Split(alt, " ") {
-				if part[0] == '"' {
-					buf.WriteString(strings.Trim(part, `"`))
-					continue
-				}
-				buf.WriteString(eval(part))
+			if f[0] == '"' {
+				chars[n] = append(chars[n], f[1])
+				continue
 			}
+			aliases[n] = append(aliases[n], do(f))
 		}
-		buf.WriteString(")")
-		res := buf.String()
-		memo[key] = res
-		return res
+
+		return n
+	}
+	do("0")
+
+	// Eliminate unit rules.
+	var elim func(int)
+	elim = func(key int) {
+		for _, val := range aliases[key] {
+			elim(val)
+			chars[key] = append(chars[key], chars[val]...)
+			pairs[key] = append(pairs[key], pairs[val]...)
+		}
+		aliases[key] = nil
+	}
+	for key := range aliases {
+		elim(key)
 	}
 
-	return regexp.MustCompile("^" + eval("0") + "$")
+	return chomsky{chars, pairs}
+}
+
+func match(rules chomsky, message string) bool {
+	const N = 100
+	if len(message) >= N {
+		log.Fatal(message)
+	}
+
+	const M = 200
+	if len(rules.chars) > M {
+		log.Fatal("rules length", len(rules.chars))
+	}
+
+	// CYK algorithm
+
+	// m is indexed: [start][end][rule]
+	var m [N][N][M]bool
+
+	for start := 0; start < len(message); start++ {
+		for rule, chars := range rules.chars {
+			if bytes.IndexByte(chars, message[start]) >= 0 {
+				m[start][start+1][rule] = true
+			}
+		}
+	}
+
+	for w := 2; w <= len(message); w++ {
+		for start := 0; start+w <= len(message); start++ {
+			end := start + w
+		Rules:
+			for rule, pairs := range rules.pairs {
+				for _, pair := range pairs {
+					for mid := start + 1; mid < end; mid++ {
+						if m[start][mid][pair[0]] && m[mid][end][pair[1]] {
+							m[start][end][rule] = true
+							continue Rules
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return m[0][len(message)][0]
 }
